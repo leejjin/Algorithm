fdd5a0411f67c3eec0a8a9f9ef2bdcef4f1ca034
YUJIN <leezle423@gmail.com>
2020-10-21 22:40:58 +0900

    201021_BOJ15688_수정렬하기5

---
+++ BOJ/BOJ15688_수정렬하기5.java
@@ -0,0 +1,28 @@
+package day1021;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ15688_수정렬하기5 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] nums = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(nums);
+
+		for (int j = 0; j < n; j++) {
+			sb.append(nums[j]).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
