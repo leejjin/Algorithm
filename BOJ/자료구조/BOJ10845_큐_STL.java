d9d89d61e734d00769b307dc25cba6be55ec177c
leejjin <leezle423@gmail.com>
2020-12-03 00:32:31 +0900

    silver4_BOJ10845_큐

---
+++ BOJ/자료구조/BOJ10845_큐_STL.java
@@ -0,0 +1,62 @@
+package day1201;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ10845_큐_STL {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		Queue<Integer> queue = new LinkedList<>();
+
+		int num = 0;
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			String op = st.nextToken();
+			switch (op) {
+			case "push":
+				num = Integer.parseInt(st.nextToken());
+				queue.add(num);
+				break;
+			case "pop":
+				if (!queue.isEmpty())
+					sb.append(queue.poll() + "\n");
+				else
+					sb.append("-1\n");
+				break;
+			case "size":
+				sb.append(queue.size() + "\n");
+				break;
+			case "empty":
+				if (!queue.isEmpty())
+					sb.append("0\n");
+				else
+					sb.append("1\n");
+				break;
+			case "front":
+				if (!queue.isEmpty())
+					sb.append(queue.peek() + "\n");
+				else
+					sb.append("-1\n");
+				break;
+			case "back":
+				if (!queue.isEmpty())
+					sb.append(num + "\n");
+				else
+					sb.append("-1\n");
+				break;
+
+			default:
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+}
