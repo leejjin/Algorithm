178abe0361a563687f8d740e0943164411987950
leejjin <leezle423@gmail.com>
2020-12-18 23:16:44 +0900

    silver5_BOJ17478_재귀함수가뭔가요

---
+++ BOJ/재귀/BOJ17478_재귀함수가뭔가요.java
@@ -0,0 +1,45 @@
+package day1215;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ17478_재귀함수가뭔가요 {
+
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static String underbar = "";
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+		answer(N);
+		System.out.println(sb);
+	}
+
+	private static void answer(int n) {
+
+		String temp = underbar;
+
+		// -----반복---------
+		String s1 = "\"재귀함수가 뭔가요?\"\n";
+		String s2 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+		String s3 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+		String s4 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+		// -----------------
+		String s5 = "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+		String s6 = "라고 답변하였지.\n";
+
+		if (n == 0) {
+			sb.append(temp + s1 + temp + s5 + temp + s6);
+			return;
+		}
+
+		sb.append(temp + s1 + temp + s2 + temp + s3 + temp + s4);
+		underbar += "____";
+		answer(n - 1);
+		sb.append(temp + s6);
+	}
+}
